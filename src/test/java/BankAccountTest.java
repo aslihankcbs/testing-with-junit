f0f43dede77c4a1b6cd99035411c4e2ac9a95a12
aslihan.kocabas <aslihank@cyberwise.com.tr>
2022-06-08 22:48:38 +0300

    added test cases

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Test BankAccount class")
 class BankAccountTest {
@@ -22,49 +20,5 @@ class BankAccountTest {
         BankAccount bankAccount = new BankAccount(400, 0);
         bankAccount.deposit(500);
         assertEquals(900, bankAccount.getBalance());
-    }
-
-    @Test
-    @DisplayName("Withdraw will become negative")
-    void testWithdrawNotStuckAtZero() {
-        BankAccount bankAccount = new BankAccount(500, -1000);
-        bankAccount.withdraw(800);
-        assertNotEquals(0, bankAccount.getBalance());
-    }
-
-    @Test
-    @DisplayName("Test activation account after creation")
-    void testActive() {
-        BankAccount bankAccount = new BankAccount(500, -1000);
-        assertTrue(bankAccount.isActive());
-    }
-
-    @Test
-    @DisplayName("Test set holder name")
-    void testHolderNameSet() {
-        BankAccount bankAccount = new BankAccount(500, 0);
-        bankAccount.setHolderName("Aslihan");
-        assertNotNull(bankAccount.getHolderName());
-    }
-
-    @Test
-    @DisplayName("Test that we can't withdraw below minimum")
-    void testNoWithdrawBelowMinimum() {
-        BankAccount bankAccount = new BankAccount(500, -1000);
-        assertThrows(RuntimeException.class, () -> bankAccount.withdraw(2000));
-    }
-
-    @Test
-    @DisplayName("Test no exceptions for withdraw and deposit")
-    void testWithdrawAndDepositWithoutException() {
-        BankAccount bankAccount = new BankAccount(500, -1000);
-        assertAll(() -> bankAccount.deposit(200), () -> bankAccount.withdraw(450));
-    }
-
-    @Test
-    @DisplayName("Test no exceptions for withdraw and deposit")
-    void testDepositTimeout() {
-        BankAccount bankAccount = new BankAccount(400, 0);
-        assertTimeout(Duration.ofNanos(1), () -> bankAccount.deposit(200));
     }
 }
